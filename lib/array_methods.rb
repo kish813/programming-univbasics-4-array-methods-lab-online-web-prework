b8ed53a32fc55555bce69120d60381a129e3c0e2
Lakisha Aldridge <lakishaaldridge@gmail.com>
2019-09-25 00:55:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,7 @@
 def using_include(array, element)
-
+famous_singers = ["beyonce", "rihanna", "shakira", "kehlani"]
+famous_singers.include? ("taylor swift") => true
+famous_singers.include? ("beyonce") => true
 end
 
 def using_sort(array)
